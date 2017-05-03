e4af67720402738870536fd1bef474438292746d
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-05-03 01:27:14 -0700

    added search bar and animated both icon and search bar

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
-	
+	// opens random wiki article
+	$('#random-article').on('click',function(){
+		window.open('https://en.wikipedia.org/wiki/Special:Random');	
+	})
+	// reveals search bar and hides search icon
+	$('.fa-search').on('click', function(){
+		$('.search-bar').show('slow');
+		$('.fa-search').hide('fast');
+    })
+	// hides search bar and shows search icon
+    $('#btn-x').on('click', function(){
+    	$('.search-bar').hide('slow');
+    	$('.fa-search').show('fast');
+    })
 
 
-
-
-
-
-	
 });
