3e8a21a46b12c1c00ff6b61f0932b6eac6147899
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-05-22 16:47:34 -0700

    added a go button to search

--- script.js
+++ script.js
@@ -14,61 +14,68 @@ $(document).ready(function(){
     	$('.fa-search').show('fast');
     })
     //create an ajax call to get wiki data on search value upon key press
+    var runAjax = function(){
+        var searchVal = $('#search-form').val();
+        var wikiUrl = 'https://en.wikipedia.org/w/api.php?';
+        $.ajax({
+            url: wikiUrl,
+            data: 
+            {
+                "action": "query",
+                "format": "json",
+                "origin": "*",
+                "prop": "info|extracts",
+                "list": "search",
+                "titles": searchVal,
+                "generator": "search",
+                "inprop": "url",
+                "exsentences": "3",
+                "exlimit": "10",
+                "exintro": 1,
+                "srsearch": searchVal,
+                "srlimit": "10",
+                "gsrsearch": searchVal,
+                "gsrprop": "size|wordcount|timestamp|snippet"
+            },
+            headers: 
+            {
+                'Api-User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+            },
+            success: function(response){
+                console.log(response);
+
+                for (var properties in response.query.pages) {
+                    $('#results-container')
+                        .prepend('<div class="results-div"><a target="_blank" href="' + response.query.pages[properties].fullurl + '"><p id="respTitle">' + response.query.pages[properties].title + '</p></a>' + '<d class="search-description">' + response.query.pages[properties].extract + '</div></div>')
+                }
+                    
+                $('#wiki-container').hide('slow');
+
+                $('.results-div').fadeIn('slow');
+                        
+
+                $('#backButton').append('<button class="btn btn-md btn-danger btnBack">Go Back</button>');
+
+                $('.btnBack').on('click', function(){
+                    $('.results-div').fadeOut('slow');
+                    $('#wiki-container').show('slow');
+                    $('#backButton').hide('fast');
+                    location.reload();
+                });
+            }
+        })
+    }
+    //press enter to run search
     $('#search-form').keypress(function(e){
-    	var searchVal = $('#search-form').val();
-    	var wikiUrl = 'https://en.wikipedia.org/w/api.php?';
+
     	if (e.which === 13) {
-    		$.ajax({
-                url: wikiUrl,
-                data: 
-                {
-                    "action": "query",
-                    "format": "json",
-                    "origin": "*",
-                    "prop": "info|extracts",
-                    "list": "search",
-                    "titles": searchVal,
-                    "generator": "search",
-                    "inprop": "url",
-                    "exsentences": "3",
-                    "exlimit": "10",
-                    "exintro": 1,
-                    "srsearch": searchVal,
-                    "srlimit": "10",
-                    "gsrsearch": searchVal,
-                    "gsrprop": "size|wordcount|timestamp|snippet"
-                },
-    			headers: 
-                {
-                    'Api-User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
-                },
-    			success: function(response){
-    				console.log(response);
-                    
-                    //loop through API object
-                    for (var properties in response.query.pages) {
-                        $('#results-container')
-                            .prepend('<div class="results-div"><a target="_blank" href="' + response.query.pages[properties].fullurl + '"><p id="respTitle">' + response.query.pages[properties].title + '</p></a>' + '<d class="search-description">' + response.query.pages[properties].extract + '</div></div>')
-                    }
-                    
-                    $('#wiki-container').hide('slow');
-
-                    $('.results-div').fadeIn('slow');
-            			
-
-					$('#backButton').append('<button class="btn btn-md btn-danger btnBack">Go Back</button>');
-
-					$('.btnBack').on('click', function(){
-						$('.results-div').fadeOut('slow');
-						$('#wiki-container').show('slow');
-						$('#backButton').hide('fast');
-						location.reload();
-					});
-    			}
-    		})
+    		runAjax();
     		return false;
     	}
     })
-
+    //click go to run search
+    $('#btn-go').on('click',function(){
+        runAjax();
+    })
 
 });
