0cbf4760250edb68e06ec3ffee5f7d2ba0791bba
Daniel Nguyen <danielnguyen93@hotmail.com>
2017-05-23 11:34:15 -0700

    added a switch function to disable multiple calls

--- script.js
+++ script.js
@@ -1,81 +1,88 @@
 $(document).ready(function(){
-	// opens random wiki article
-	$('#random-article').on('click',function(){
-		window.open('https://en.wikipedia.org/wiki/Special:Random');	
-	})
-	// reveals search bar and hides search icon
-	$('.fa-search').on('click', function(){
-		$('.search-bar').show('fast');
-		$('.fa-search').hide('fast');
+  // opens random wiki article
+  $('#random-article').on('click',function(){
+    window.open('https://en.wikipedia.org/wiki/Special:Random');	
+  })
+
+  // reveals search bar and hides search icon
+  $('.fa-search').on('click', function(){
+    $('.search-bar').show('fast');
+    $('.fa-search').hide('fast');
+  })
+
+  // hides search bar and shows search icon
+  $('#btn-x').on('click', function(){
+    $('.search-bar').hide('fast');
+    $('.fa-search').show('fast');
+  })
+
+  //create an ajax call to get wiki data on search value upon key press
+  let runAjax = function(){
+    ajaxSwitch = false;
+    var searchVal = $('#search-form').val();
+    var wikiUrl = 'https://en.wikipedia.org/w/api.php?';
+    $.ajax({
+      url: wikiUrl,
+      data: 
+      {
+        "action": "query",
+        "format": "json",
+        "origin": "*",
+        "prop": "info|extracts",
+        "list": "search",
+        "titles": searchVal,
+        "generator": "search",
+        "inprop": "url",
+        "exsentences": "3",
+        "exlimit": "10",
+        "exintro": 1,
+        "srsearch": searchVal,
+        "srlimit": "10",
+        "gsrsearch": searchVal,
+        "gsrprop": "size|wordcount|timestamp|snippet"
+      },
+      headers: 
+      {
+        'Api-User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+      },
+      success: function(response){
+        console.log(response);
+
+        for (var properties in response.query.pages) {
+          $('#results-container')
+            .prepend('<div class="results-div"><a target="_blank" href="' + response.query.pages[properties].fullurl + '"><p id="respTitle">' + response.query.pages[properties].title + '</p></a>' + '<d class="search-description">' + response.query.pages[properties].extract + '</div></div>')
+        }
+
+        $('#wiki-container').hide('slow');
+
+        $('.results-div').fadeIn('slow');
+
+
+        $('#backButton').append('<button class="btn btn-md btn-danger btnBack">Go Back</button>');
+
+        $('.btnBack').on('click', function(){
+          $('.results-div').fadeOut('slow');
+          $('#wiki-container').show('slow');
+          $('#backButton').hide('fast');
+          location.reload();
+        });
+      }
     })
-	// hides search bar and shows search icon
-    $('#btn-x').on('click', function(){
-    	$('.search-bar').hide('fast');
-    	$('.fa-search').show('fast');
-    })
-    //create an ajax call to get wiki data on search value upon key press
-    var runAjax = function(){
-        var searchVal = $('#search-form').val();
-        var wikiUrl = 'https://en.wikipedia.org/w/api.php?';
-        $.ajax({
-            url: wikiUrl,
-            data: 
-            {
-                "action": "query",
-                "format": "json",
-                "origin": "*",
-                "prop": "info|extracts",
-                "list": "search",
-                "titles": searchVal,
-                "generator": "search",
-                "inprop": "url",
-                "exsentences": "3",
-                "exlimit": "10",
-                "exintro": 1,
-                "srsearch": searchVal,
-                "srlimit": "10",
-                "gsrsearch": searchVal,
-                "gsrprop": "size|wordcount|timestamp|snippet"
-            },
-            headers: 
-            {
-                'Api-User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
-            },
-            success: function(response){
-                console.log(response);
+  }
+  let ajaxSwitch = true;
+  //press enter to run search
+  $('#search-form').keypress(function(e){
 
-                for (var properties in response.query.pages) {
-                    $('#results-container')
-                        .prepend('<div class="results-div"><a target="_blank" href="' + response.query.pages[properties].fullurl + '"><p id="respTitle">' + response.query.pages[properties].title + '</p></a>' + '<d class="search-description">' + response.query.pages[properties].extract + '</div></div>')
-                }
-                    
-                $('#wiki-container').hide('slow');
-
-                $('.results-div').fadeIn('slow');
-                        
-
-                $('#backButton').append('<button class="btn btn-md btn-danger btnBack">Go Back</button>');
-
-                $('.btnBack').on('click', function(){
-                    $('.results-div').fadeOut('slow');
-                    $('#wiki-container').show('slow');
-                    $('#backButton').hide('fast');
-                    location.reload();
-                });
-            }
-        })
+    if (e.which === 13 && ajaxSwitch) {
+      runAjax();
+      return false;
     }
-    //press enter to run search
-    $('#search-form').keypress(function(e){
-
-    	if (e.which === 13) {
-    		runAjax();
-    		return false;
-    	}
-    })
-    //click go to run search
-    $('#btn-go').on('click',function(){
-        runAjax();
-    })
-
+  })
+  //click go to run search
+  $('#btn-go').on('click',function(){
+    if (ajaxSwitch) {
+      runAjax();
+      return false;
+    }  
+  })
 });
